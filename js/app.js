9606b8906dd9a3da1115c97a4b9c5f616f0a1855
Georgi Naumov <gonaumov@gmail.com>
2014-11-08 01:19:38 +0200

    Add routing and many improvements.

---
+++ js/app.js
@@ -0,0 +1,34 @@
+/* App Module */
+
+var carDealerShipApp = angular.module('carDealerShipApp', [
+    'ngRoute',
+    'carDealerShipAppControllers'
+]);
+
+carDealerShipApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/home', {
+                templateUrl: 'partials/home.html',
+                controller: 'HomeCtrl'
+            }).
+            when('/availableCars', {
+                templateUrl: 'partials/availableCars.html',
+                controller: 'AvailableCarsCtrl'
+            }).
+            when('/services', {
+                templateUrl: 'partials/services.html',
+                controller: 'ServicesCtrl'
+            }).
+            when('/contact', {
+                templateUrl: 'partials/contact.html',
+                controller: 'ContactCtrl'
+            }).
+            when('/administration', {
+                templateUrl: 'partials/administration.html',
+                controller: 'AdministrationCtrl'
+            }).
+            otherwise({
+                redirectTo: '/home'
+            });
+    }]);
---
+++ js/controllers.js
@@ -0,0 +1,34 @@
+/* Controllers */
+
+var carDealerShipAppControllers = angular.module('carDealerShipAppControllers', []);
+
+carDealerShipAppControllers.controller('MainController', function($scope, $route) {
+    $scope.$route = $route;
+})
+
+carDealerShipAppControllers.controller('HomeCtrl', ['$scope',
+    function ($scope) {
+        $scope.name = 'HomeCtrl';
+
+    }]);
+
+
+carDealerShipAppControllers.controller('AvailableCarsCtrl', ['$scope',
+    function ($scope) {
+        $scope.name = 'AvailableCarsCtrl';
+    }]);
+
+carDealerShipAppControllers.controller('ServicesCtrl', ['$scope',
+    function ($scope) {
+        $scope.name = 'ServicesCtrl';
+    }]);
+
+carDealerShipAppControllers.controller('ContactCtrl', ['$scope',
+    function ($scope) {
+        $scope.name = 'ContactCtrl';
+    }]);
+
+carDealerShipAppControllers.controller('AdministrationCtrl', ['$scope',
+    function ($scope) {
+        $scope.name = 'AdministrationCtrl';
+    }]);
